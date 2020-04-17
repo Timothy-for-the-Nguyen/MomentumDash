68eb56b89cb57ec72e708f00bdfe9fe388fa2dea
Ubuntu <ubuntu@ip-172-31-32-44.us-east-2.compute.internal>
2020-04-17 19:19:05 +0000

    Way of showing generating multiple iterations

--- app/controllers/iterations_controller.rb
+++ app/controllers/iterations_controller.rb
@@ -3,11 +3,17 @@ class IterationsController < ApplicationController
     
     def create
         @team = Team.find(params[:team_id])
-        @iterations = @team.iterations.create("status"=>"hello")
+        @value = @team.iteration_num
+        @value.times do |i| 
+            @iterations = @team.iterations.create("status"=>"Not submitted", "iteration"=>"Iteration #{i}")
+        end 
         redirect_to team_path(@team)
     end 
     
-   
+   def update
+        @team = Team.find(params[:team_id])
+        @iterations = team.iterations.update
+   end 
     
     private
         def iter_params
